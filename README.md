68bc6fb0c495951de1f6a832fa11ea7d895866e3
JackAlexanderClark <jackalexanderclark@protonmail.com>
2022-05-14 18:39:06 +0000

    1

---
+++ README.md
@@ -0,0 +1,4 @@
+### Milestone Project 2 - Interactive Frontend Application
+
+#### Police Suspect Game
+##### The premise is similar to that of cluedo.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
 //One single guess of suspect allowed
+const randomNumber = Math.floor(Math.random() * 100) + 1
+console.log('Random Number', randomNumber)
+
+// One guess gameloop
+function checkGuess() {
+  let myGuess = guess.value;
+  if (myGuess === randomNumber) {
+    feedback.textContent = "You got it right!"
+  } else if (myGuess > randomNumber) {
+    feedback.textContent = "Your guess was " + myGuess + ". That's too high. Try Again!"
+  } else if (myGuess < randomNumber) {
+   feedback.textContent = "Your guess was " + myGuess + ". That's too low. Try Again!"
+ }
+}
+submitGuess.addEventListener('click', checkGuess)
+
+// jQuery button for animating clue 01
+$("#button-1").click(function(){
+  $(this).animate(
+    left, '100px',
+    height, '100px',
+    width, '50px',
+    opacity, '0.3').fadeOut("slow").hide("slow")
+});
+
