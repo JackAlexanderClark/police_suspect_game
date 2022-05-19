5a8ebc53e3828723040639b1a9f0ead2acee7635
JackAlexanderClark <jackalexanderclark@protonmail.com>
2022-05-19 12:37:28 +0000

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,34 @@
 //Defining the constant true suspect
 const correctSuspect = suspect8;
+let attempts = 0;
 // One guess gameloop
 document.getElementById("guessChoice").onclick = checkGuess();
 
 function checkGuess() {
   let playerGuess = guess.value;
+  if (attempts === 3) {
+    feedback.textContent = "Game over! Failed. You have used all 3 guesses!";
+    //Need to restart the game using the restartGame() function
+
+    return; 
+  }
   if (playerGuess === suspect8) {
+    attempts = 0;
     feedback.textContent = "You got it right! Suspect 8 was the criminal who killed your family!";
     console.log("CORRECT ANSWER");
   } else if (myGuess != correctSuspect) {
+    attempts += 1;
     feedback.textContent = "Your guess was" + playerGuess + ". That's incorrect, you selected an inoccent man. Try Again!"
     console.log("INCORRECT ANSWER");
 };
+
+function restartGame() {
+  //
+}
+
+function incorrectGuess() {
+  //-1 from 3 guesses
+}
 
 //Incrementing wrong answer score
 let wrongAnswer = myGuess != correctSuspect;
@@ -36,7 +53,7 @@ function changeColor(newColor) {
 
 // Method chaining jQuery paragraph hiding
 $("#paragraph-1").click(function () {
-  $(this).(#button).hide(2500).show(1000)
+  $(this).fadeOut("#button").hide(2500).show(1000)
 });
 
 // Mouse enter and leave jQuery
